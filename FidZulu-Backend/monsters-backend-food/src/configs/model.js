4b376ab9a0cb1e0246ef46c8947d007614da814f
Associate <Associate@roifmrwindt>
2023-10-12 11:00:52 +0530

    Changed db.js food.json model.js teams.json

--- FidZulu-Backend/monsters-backend-food/src/configs/model.js
+++ FidZulu-Backend/monsters-backend-food/src/configs/model.js
@@ -1,30 +1,27 @@
-// const db = require('./db');
+const db = require('./db');
 const fs = require('fs');
 const path = require('path');
 const oracledb = require('oracledb');
-// const user=process.env.USER;
-// const password=process.env.PASSWORD;
-// const string=process.env.CONNECTION_STRING;
-// const oracleXeConnString = `
-//   (DESCRIPTION =
-//     (ADDRESS = (PROTOCOL = TCP)(HOST = LOCALHOST)(PORT = 1521))
-//     (CONNECT_DATA =
-//       (SERVER = DEDICATED)
-//       (SERVICE_NAME = XEPDB1)
-//     )
-//   )
-// `;
+const user=process.env.USER;
+const password=process.env.PASSWORD;
+const string=process.env.CONNECTION_STRING;
+const oracleXeConnString = `
+  (DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = LOCALHOST)(PORT = 1521))
+    (CONNECT_DATA =
+      (SERVER = DEDICATED)
+      (SERVICE_NAME = XEPDB1)
+    )
+  )
+`;
 
 module.exports = async (query, data = '') => {
-    // const connection = await oracledb.getConnection({ 
-    //     user: user, 
-    //     password: password, 
-    //     connectionString: string
-    // }).then(console.log("connected successfully"));
-    // console.log((await connection.execute(query)).rows);
-    // return (await connection.execute(query)).rows;
-    const filePath = path.join(__dirname, 'food.json');
-    const jsonData = fs.readFileSync(filePath, 'utf8');
-    const foodData = JSON.parse(jsonData);
-    return foodData;
+    const connection = await oracledb.getConnection({ 
+        user: user, 
+        password: password, 
+        connectionString: string
+    }).then(console.log("connected successfully"));
+    console.log((await connection.execute(query)).rows);
+    return (await connection.execute(query)).rows;
+   
 };
