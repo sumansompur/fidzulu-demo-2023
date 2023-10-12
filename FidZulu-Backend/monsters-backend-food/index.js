857fe179576ceacca2a74e268ca7e62bbc3221d7
Associate <Associate@roifmrwindt>
2023-10-12 15:29:26 +0530

    Added DockerFiles

--- FidZulu-Backend/monsters-backend-food/index.js
+++ FidZulu-Backend/monsters-backend-food/index.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const foodRouter = require('./src/routes/foodRoute');
-const teamsRouter = require('./src/routes/teamsRoute')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const foodRouter = require("./src/routes/foodRoute");
+const teamsRouter = require("./src/routes/teamsRoute");
 const connection = require("./src/configs/db");
 
 const app = express();
-const port = process.env.PORT || 3032;
+const port = process.env.FOOD_PORT || 3032;
 // connection.getConnection();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(foodRouter);
 app.use(teamsRouter);
-app.get('/', foodRouter);
-app.get('/',teamsRouter);
-app.listen(port, '0.0.0.0', () => {
+app.get("/", foodRouter);
+app.get("/", teamsRouter);
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
